02d34f464f4f0b16f254a77d64b05abb919e60fe
Siyeong Cheon <108331642+1000c0@users.noreply.github.com>
2023-09-20 02:04:08 +0900

    LinkedList_Lec

--- Code in Lecture/ImplementationNode.py
+++ Code in Lecture/ImplementationNode.py
@@ -53,7 +53,7 @@ class Node:
         return return_str
 
     def __repr__(self) -> str:
-        return_str = f"Node : {self._data}"
+        return_str = f"Node({self._data})"
         return return_str
         # self._next 로 할 경우
         # I have a data : 2
@@ -63,3 +63,138 @@ class Node:
 
     def __add__(self, other) -> None:
         self._next = other
+
+
+class Vector:
+    def __init__(self, *args):
+        self.args = args
+
+    def __add__(self, other):
+        a = [arg1 + arg2 for arg1, arg2 in zip(self.args, other.args)]
+
+    def __repr__(self):
+        return self.__class__.__name__ + str(self.args)
+
+
+v1 = Vector(1, 4, 8, 9)
+v2 = Vector(5, 3, 2, 1)
+
+
+class LinkedListBag(object):
+    """_summary_
+
+    Args:
+        object (_type_): _description_
+    """
+
+    def __init__(self, first_node: Node = None) -> None:
+        super().__init__()
+        self._head = first_node
+        self._tail = first_node
+        if first_node is None:
+            self.size = 0
+        else:
+            self.size = self._count(self)
+
+    def __contains__(self, target: int):
+        cur_node = self.head
+        while cur_node is not None:
+            if cur_node.data == target:
+                return True
+            else:
+                return False
+
+    def _count(self) -> int:
+        counter = 0
+        cur_node = self._head
+        while cur_node is not None:
+            counter += 1
+            cur_node = cur_node.next
+        return counter
+
+    def append(self, new_node: Node) -> bool:  # O(1) -> tail 끝에 붙히기 때문
+        try:
+            if self.size == 0:
+                self._head = new_node
+                self._tail = new_node
+            else:
+                self._tail.next = new_node
+                self._tail = new_node
+            self.size += 1
+            return True
+        except Exception as e:
+            print(e)
+            return False
+
+    def insert(self, new_node: Node, index_number: int) -> bool:
+        index_counter = 0
+        cur_node = self._head
+
+        if index_number == 0:
+            new_node.next = self._head
+            self._head = new_node
+            self.size += 1
+            return True
+
+        while cur_node is not None:
+            if index_number == index_counter:
+                pred_node.next = new_node
+                new_node.next = cur_node
+                self.size += 1
+                return True
+            else:
+                pred_node = cur_node
+                cur_node = cur_node.next
+                index_counter += 1
+        return False
+
+    def remove(self, target_value: int) -> bool:
+        cur_node = self._head
+
+        while cur_node is not None:
+            if cur_node == target_value:
+                pred_node.next = cur_node.next
+                del cur_node
+                self.size -= 1
+                return True
+            else:
+                pred_node = cur_node
+                cur_node = cur_node.next
+        return False
+
+    def __len__(self) -> int:
+        return self.size
+
+    def __repr__(self) -> str:
+        cur_node = self.head
+        if self.size == 0:
+            return None
+        return_str = ""
+        while cur_node is not None:
+            return_str += f" {cur_node.data} -> "
+            cur_node = cur_node.next
+        return_str += f"End of Linked List"
+        return return_str
+
+    def __iter__(self):
+        return _BagIterator(self._head)
+
+    @property
+    def head(self) -> Node:
+        return self._head
+
+
+class _BagIterator:
+    def __init__(self, head_node) -> None:
+        self._cur_node = head_node
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self._cur_node is None:
+            raise StopIteration
+        else:
+            node = self._cur_node
+            self._cur_node = self._cur_node.next
+            return node
